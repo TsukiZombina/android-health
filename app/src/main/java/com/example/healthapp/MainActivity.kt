e659fb6ecf8a92e2fdacd49e0055624def52c6e1
MM <zmmarquez@minsait.com>
2023-05-21 22:31:07 -0600

    Remove MenuActivity.kt

--- app/src/main/java/com/example/healthapp/MainActivity.kt
+++ app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //hadle click login
+        //handle click login
         binding.loginBtn.setOnClickListener {
 
         }
--- app/src/main/java/com/example/healthapp/SplashActivity.kt
+++ app/src/main/java/com/example/healthapp/SplashActivity.kt
@@ -11,7 +11,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed(Runnable {
-            startActivity(Intent(this, MenuActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000)
     }
